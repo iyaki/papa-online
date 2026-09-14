ca4034c0838fe0fa54cf7caabb89cf10c649af1a
iyaki <me@iyaki.ar>
2026-09-14 23:12:31 +0000

    build: inject app version into images via docker build args

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -2,4 +2,7 @@
 
 SCRIPTPATH=$(dirname "$(realpath "$0")")
 
-docker build --tag ghcr.io/iyaki/papa-online:latest --push ${SCRIPTPATH}/..
+docker build \
+    --build-arg APP_VERSION="$(git -C "${SCRIPTPATH}/.." rev-parse --short HEAD)" \
+    --build-arg APP_BUILT_AT="$(date -u +%Y-%m-%dT%H:%M:%SZ)" \
+    --tag ghcr.io/iyaki/papa-online:latest --push "${SCRIPTPATH}/.."
