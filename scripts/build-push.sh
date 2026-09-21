c5ba6b05642aa457876d19bbeab5448c378a6ae8
iyaki <me@iyaki.ar>
2026-09-21 15:28:14 -0300

    separate docker push command from build for clarity

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -5,4 +5,6 @@ SCRIPTPATH=$(dirname "$(realpath "$0")")
 docker build \
     --build-arg APP_VERSION="$(git -C "${SCRIPTPATH}/.." rev-parse --short HEAD)" \
     --build-arg APP_BUILT_AT="$(date -u +%Y-%m-%dT%H:%M:%SZ)" \
-    --tag ghcr.io/iyaki/papa-online:latest --push "${SCRIPTPATH}/.."
+    --tag ghcr.io/iyaki/papa-online:latest "${SCRIPTPATH}/.."
+
+docker push ghcr.io/iyaki/papa-online:latest
